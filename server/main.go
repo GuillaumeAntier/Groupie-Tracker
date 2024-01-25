c0e4e5aa0411e48a9f51a6149f99c7dc4550906a
Guillaume Antier <guillaume.antier@ynov.com>
2024-01-25 09:08:22 +0100

    Add a server avaible

--- server/main.go
+++ server/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HandlerMain(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "HTML/index.html")
+}
+
+func main() {
+
+	http.HandleFunc("/", HandlerMain)
+
+	fmt.Println("Server is listening...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
