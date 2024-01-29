e73b4e54f95a62035bf8dc4bc73c75e42340a0d3
Paul Decatoire <decatopaul@gmail.com>
2024-01-29 09:23:52 +0100

    Reprise de la partie de Guillaume afin de bien repartir

--- server/main.go
+++ server/main.go
@@ -6,13 +6,13 @@ import (
 	//"bufio"
 	"html/template"
 	"io/ioutil"
+	//"encoding/json"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 const port = ":8080"
-
 var response string
 var respart []string
 var resploc []string
@@ -23,50 +23,9 @@ var name string
 var artist Artist
 
 type Artist struct{
-	ID string
 	Image string
-	//Name string
-	//Members []string
-	//CreationDate int
-	//FirstAlbum string
+	Name string
 }
-/*type Locations struct{
-	index []string
-	id string
-	locations string
-	dates string
-}
-type Dates struct{
-	index []string
-	id string
-	dates string
-}
-type Relations struct{
-	index []string
-	id string
-	datesLocations string
-}*//*
-func data_art(w http.ResponseWriter, r *http.Request) {
-	urlart := "https://groupietrackers.herokuapp.com/api/artists"
-	respart, err := http.Get(urlart)
-	if err != nil {
-		fmt.Println("Erreur lors de la requête HTTP:", err)
-		return
-	}
-	defer respart.Body.Close()
-	bodyart, err := ioutil.ReadAll(respart.Body)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture:", err)
-		return 
-	}
-	//fmt.Println(string(bodyart))
-	var artist []Artist
-	err = json.Unmarshal(bodyart, &artist)
-	fmt.Println(artist)
-	servePage(w, r, "index.html", artist)
-	
-}*/
-
 func data_loc() {
 	urlloc := "https://groupietrackers.herokuapp.com/api/locations"
 	resploc, err := http.Get(urlloc)
@@ -80,12 +39,7 @@ func data_loc() {
 		fmt.Println("Erreur lors de la lecture:", err)
 		return 
 	}
-	
 	fmt.Println(string(bodyloc))
-	//var locations []Locations
-	//err = json.Unmarshal(bodyloc, &locations)
-	//fmt.Println(string(locations))
-	
 	
 }
 func data_dat() {
@@ -101,10 +55,6 @@ func data_dat() {
 		fmt.Println("Erreur lors de la lecture:", err)
 		return 
 	}
-	
-	//var dates []Dates
-	//err = json.Unmarshal(bodydat, &dates)
-	//fmt.Println(string(dates))
 	fmt.Println(string(bodydat))
 	
 }
@@ -121,9 +71,6 @@ func data_rel() {
 		fmt.Println("Erreur lors de la lecture:", err)
 		return 
 	}
-	//var relations []Relations
-	//err = json.Unmarshal(bodyrel, &relations)
-	//fmt.Println(string(relations))
 	fmt.Println(string(bodyrel))
 	
 }
@@ -165,13 +112,9 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	research := r.URL.Query().Get("research")
 	fmt.Println(research)
 
-	
-	
-	
 }
 
 func main() {
-	//data_art()
 	data_loc()
 	data_dat()
 	data_rel()
@@ -182,5 +125,5 @@ func main() {
 	fmt.Println("Server is listening...")
 	fmt.Println("(http://localhost:8080) - Serveur démarré sur le port", port)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	
 }
