016579804bd15e8f105c72f26fd4946072fb79cd
Paul Decatoire <decatopaul@gmail.com>
2024-02-15 09:07:33 +0100

    Changement de noms pour les html

--- server/main.go
+++ server/main.go
@@ -1,139 +1,170 @@
 package main
 
 import (
-
-	//"os"
-	//"bufio"
-	"html/template"
-	"io/ioutil"
-	//"encoding/json"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
-const port = ":8080"
-var response string
-var respart []string
-var resploc []string
-var respdat []string
-var resprel []string
-var err []string
-var name string
-var artist Artist
 
-type Interface struct{
-	
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
 }
-type Artist struct{
-	Image string
-	Name string
+
+type Event struct {
+	DatesLocations map[string][]string `json:"datesLocations"`
 }
-type Localisation struct{
-	//index []string
-	//id string
-	Localisations string
-	Dates string
+
+type Relation struct {
+	Artist []Artist
+	Event  []Event
 }
-func data_loc() {
-	urlloc := "https://groupietrackers.herokuapp.com/api/locations"
-	resploc, err := http.Get(urlloc)
+
+type LocationCity struct {
+	Latitude  string `json:"lat"`
+	Longitude string `json:"lon"`
+}
+
+func openAPI(url string) ([]byte, error) {
+	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Erreur lors de la requête HTTP:", err)
-		return
+		return nil, err
 	}
-	defer resploc.Body.Close()
-	bodyloc, err := ioutil.ReadAll(resploc.Body)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println("Erreur lors de la lecture:", err)
-		return 
+		return nil, err
 	}
-	fmt.Println(string(bodyloc))
-	
+	return body, nil
 }
-func data_dat() {
-	urldat := "https://groupietrackers.herokuapp.com/api/dates"
-	respdat, err := http.Get(urldat)
+
+func filterArtistsByLetter(artists []Artist, letter string) []Artist {
+	filteredArtists := make([]Artist, 0)
+	for _, artist := range artists {
+		if strings.HasPrefix(strings.ToLower(artist.Name), strings.ToLower(letter)) {
+			filteredArtists = append(filteredArtists, artist)
+		}
+	}
+	return filteredArtists
+}
+
+func servePageArtist(w http.ResponseWriter, r *http.Request, html string, data []Artist) {
+	page, err := template.ParseFiles("html/" + html)
 	if err != nil {
-		fmt.Println("Erreur lors de la requête HTTP:", err)
+		http.NotFound(w, r)
 		return
-	}
-	defer respdat.Body.Close()
-	bodydat, err := ioutil.ReadAll(respdat.Body)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture:", err)
-		return 
-	}
-	fmt.Println(string(bodydat))
-	
-}
-func data_rel() {
-	urlrel := "https://groupietrackers.herokuapp.com/api/relation"
-	resprel, err := http.Get(urlrel)
-	if err != nil {
-		fmt.Println("Erreur lors de la requête HTTP:", err)
-		return
-	}
-	defer resprel.Body.Close()
-	bodyrel, err := ioutil.ReadAll(resprel.Body)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture:", err)
-		return 
-	}
-	fmt.Println(string(bodyrel))
-	
-}
-func servePage (w http.ResponseWriter, r *http.Request, html string, data []Artist) {
-	page,err := template.ParseFiles("HTML/"+html)
-	if err != nil {
-		fmt.Println(err)
 	}
 	err = page.Execute(w, data)
 	if err != nil {
-		fmt.Println(err)
-	}
-
-}
-func HandlerMain(w http.ResponseWriter, r *http.Request) {
-	urlart := "https://groupietrackers.herokuapp.com/api/artists"
-	respart, err := http.Get(urlart)
-	if err != nil {
-		fmt.Println("Erreur lors de la requête HTTP:", err)
+		http.NotFound(w, r)
 		return
 	}
-	defer respart.Body.Close()
-	bodyart, err := ioutil.ReadAll(respart.Body)
+}
+
+func servePage(w http.ResponseWriter, r *http.Request, html string, data Event) {
+	page, err := template.ParseFiles("html/" + html)
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture:", err)
-		return 
+		http.NotFound(w, r)
+		return
 	}
+	err = page.Execute(w, data)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+}
+
+func HandlerMain(w http.ResponseWriter, r *http.Request) {
+	url := "https://groupietrackers.herokuapp.com/api/"
+	bodyart, err := openAPI(url + "artists")
 	var artist []Artist
 	err = json.Unmarshal(bodyart, &artist)
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture:", err)
+		http.NotFound(w, r)
 		return
 	}
-	servePage(w, r, "accueil.html", artist)
+	servePageArtist(w, r, "index.html", artist)
 }
 
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
+func searchHandler(w http.ResponseWriter, r *http.Request) {
 	research := r.URL.Query().Get("research")
-	fmt.Println(research)
+	url := "https://groupietrackers.herokuapp.com/api/"
+	bodyart, err := openAPI(url + "artists")
+	var artist []Artist
+	err = json.Unmarshal(bodyart, &artist)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	artist = filterArtistsByLetter(artist, research)
+	servePageArtist(w, r, "result.html", artist)
+}
 
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	url := "https://groupietrackers.herokuapp.com/api/"
+	bodyevent, err := openAPI(url + "relation/" + id)
+	var event Event
+	err = json.Unmarshal(bodyevent, &event)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	for _, locations := range event.DatesLocations {
+		latitude, longitude, err := getCoordinates(locations[0])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Println("Latitude:", latitude, "Longitude:", longitude)
+	}
+	servePage(w, r, "event.html", event)
+}
+
+func getCoordinates(location string) (string, string, error) {
+	baseURL := "https://nominatim.openstreetmap.org/search"
+	params := url.Values{}
+	params.Set("q", location)
+	params.Set("format", "json")
+
+	resp, err := http.Get(baseURL + "?" + params.Encode())
+	if err != nil {
+		return "", "", err
+	}
+	defer resp.Body.Close()
+
+	var results []LocationCity
+	err = json.NewDecoder(resp.Body).Decode(&results)
+	if err != nil {
+		return "", "", err
+	}
+
+	if len(results) == 0 {
+		return "", "", fmt.Errorf("No results found for location: %s", location)
+	}
+
+	return results[0].Latitude, results[0].Longitude, nil
 }
 
 func main() {
-	data_loc()
-	data_dat()
-	data_rel()
-
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.HandleFunc("/", HandlerMain)
-	http.HandleFunc("/result", getHandler)
-	
+	http.HandleFunc("/index.html", HandlerMain)
+	http.HandleFunc("/result.html", searchHandler)
+	http.HandleFunc("/event.html", eventHandler)
 
 	fmt.Println("Server is listening...")
-	fmt.Println("(http://localhost:8080) - Serveur démarré sur le port", port)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	
+
 }
